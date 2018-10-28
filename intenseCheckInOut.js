915609934029e2fc28aa25f20765510ad2fdaad1
Kwadwo T. Nyamekye <ktn330.kn@gmail.com>
2018-10-28 19:30:40 -0400

    Add existing file

--- intenseCheckInOut.js
+++ intenseCheckInOut.js
@@ -21,90 +21,39 @@ result.style.color = "red"
 }
 }
 
+var btn = document.querySelector("#btn")
+btn.onclick = function(){
+var crew = document.querySelector("#crew").value 
+var truckId = document.querySelector("#truckId").value
+var dropOffLocation = document.querySelector("#dropOffLocation").value
+var gas = document.querySelector("#gas").value
+var shrinkQty =document.querySelector("#shrink qty")
+var blanketsQty =document.querySelector("#blankets")
+var tapeQty =document.querySelector("#tape qty")
+var dollyQty =document.querySelector("#dolly qty")
+var right =document.querySelector("#right")
+var left =document.querySelector("#left")
+var right =document.querySelector("#right")
+var left =document.querySelector("#left")
+var cabCondition =document.querySelector("#cab condition")
+var backCondition =document.querySelector("#back condition")
 
-// var btn = document.querySelector("#btn")
-
-// btn.onclick = function(){
-// 	var crew = document.querySelector("#crew").value 
-// 	var truck id = document.querySelector("#truckID").value
-// 	var drop off location = document.querySelector("#dropOfflocation").value
-// 	var gas = document.querySelector("#gas").value
-// 	var shrink qty =document.querySelector("#shrink qty")
-//     var blankets qty =document.querySelector("#blankets")
-//     var tape qty =document.querySelector("#tape qty")
-//    	var dolly qty =document.querySelector("#dolly qty")
-//    	var right =document.querySelector("#right")
-//    	var left =document.querySelector("#left")
-//    	var right =document.querySelector("#right")
-//    	var left =document.querySelector("#left")
-//    	var cab condition =document.querySelector("#cab condition")
-//    	var back condition =document.querySelector("#back condition")
-
-//  results.innerHTML = "Current truck status," + crew + "<br>" +
-//  "Crew" + crew
-//  "Truck ID: <br>" +  truck id  
-//  "Drop Off Locatiion" + drop off location
-//  "Gas" + gas
-//  "Shrink Qty" + shrink qty
-//  "Blanket Qty" + blanket qty
-//  "Tape Qty"  + tape qty
-//  "Dolly Qty" + dolly qty
-//  "Right" + right
-//  "Left" + left
-//  "Right" + right
-//  "Left" + left
-//  "Cab Condition" + cab condition
-//  "Back Condition" + back condition
-
+results.innerHTML = "Current truck status," + crew + "<br>" +
+"Crew" + crew
+"Truck ID" +  truck id  
+"Drop Off Locatiion" + drop off location
+"Gas" + gas
+"Shrink Qty" + shrink qty
+"Blanket Qty" + blanket qty
+"Tape Qty"  + tape qty
+"Dolly Qty" + dolly qty
+"Right" + right
+"Left" + left
+"Right" + right
+"Left" + left
+"Cab Condition" + cab condition
+"Back Condition" + back condition
+}
    	
-//     }
-//     if(female.checked){
-//     	gender = "female"
-//     }
-
-// 	results.innerHTML = "Thanks for registering," + name + "<br>" +
-// "Here is your information: <br>" + 
-// "Address:  " + address + "<br>"
-// "phone: " + phone + "<br>"
-// email + "<br>" +
-// "Your interest is " + interest + "<br>" + 
-// "Gender is " + gender
-// }
 
 
-
-// <script>
-
-//         $(function () {
-//             $("#lineargauge").igLinearGauge({
-//                 height: "100px",
-//                 width: "100%",
-//                 minimumValue: 0,
-//                 maximumValue: 10, 
-//                 value: 1,
-//                 needleBrush: "white",
-//                 needleOutline: "#2582a9",
-//                 isNeedleDraggingEnabled: true,
-//                 ranges: [
-//                    {
-//                        name: 'first',
-//                        startValue: 0,
-//                        endValue: 3, 
-//                    },
-//                    {
-//                        name: 'second',
-//                        startValue: 3,
-//                        endValue: 7, 
-//                    },
-//                    {
-//                        name: 'third',
-//                        startValue: 7,
-//                        endValue: 10, 
-//                    }
-//                 ], 
-//             });
-//         });
-
-//     </script>
-// </body>
-// </html> Gas Gauge Script
