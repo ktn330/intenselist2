e7647f8b0ad847a40fa8818eca4553f035c6a5ba
Kwadwo T. Nyamekye <ktn330.kn@gmail.com>
2018-10-30 14:38:07 -0400

    registration pass

--- intenseCheckInOut.js
+++ intenseCheckInOut.js
@@ -1,4 +1,4 @@
-var btn = document.querySelector("#login")
+var btn = document.querySelector("#btn")
 
 btn.onclick = function(){
 var username = document.querySelector("#username")
@@ -25,36 +25,42 @@ var btn = document.querySelector("#btn")
 btn.onclick = function(){
 var crew = document.querySelector("#crew").value 
 var truckId = document.querySelector("#truckId").value
+var pickupLocation = document.querySelector("#pickupLocation").value
 var dropOffLocation = document.querySelector("#dropOffLocation").value
-var alternateSideParking = document.querySelector("#alternateSideParking").value
-var gas = document.querySelector("#gas").value
-var shrinkQty =document.querySelector("#shrink qty")
-var blanketsQty =document.querySelector("#blankets")
-var tapeQty =document.querySelector("#tape qty")
-var dollyQty =document.querySelector("#dolly qty")
-var right =document.querySelector("#right")
-var left =document.querySelector("#left")
-var right =document.querySelector("#right")
-var left =document.querySelector("#left")
-var cabCondition =document.querySelector("#cab condition")
-var backCondition =document.querySelector("#back condition")
+var alternateSideParking = document.querySelector("#alterniteSideParking").value
+var gas = document.querySelector("#gasQty").value
+var shrinkQty =document.querySelector("#shrinkQty")
+var blanketsQty =document.querySelector("#blanketsQty")
+var tapeQty =document.querySelector("#tapeQty")
+var dollyQty =document.querySelector("#dollyQty")
+var right =document.querySelector("#rightMirror")
+var left =document.querySelector("#leftMirror")
+var right =document.querySelector("#rightLights")
+var left =document.querySelector("#leftLights")
+var cabCondition =document.querySelector("#cabCondition")
+var backCondition =document.querySelector("#backCondition")
+var exteriorTruckCondition =document.querySelector("#exteriorTruckCondition")
+var comments =document.querySelector("#comments")
 
-results.innerHTML = "Current truck status," + crew + "<br>" +
-"Crew" + crew
-"Truck ID" +  truck id  
-"Drop Off Locatiion" + drop off location
-"alternateSideParking" + alternateSideParking
-"Gas" + gas
-"Shrink Qty" + shrink qty
-"Blanket Qty" + blanket qty
-"Tape Qty"  + tape qty
-"Dolly Qty" + dolly qty
-"Right" + right
-"Left" + left
-"Right" + right
-"Left" + left
-"Cab Condition" + cab condition
-"Back Condition" + back condition
+results.innerHTML = "Current truck status: " + crew + "<br>" +
+"Crew: " + crew + "<br>" +
+"TruckId: " +  truckId + "<br>" +
+"PickupLocation: " + pickupLocation + "<br>" + 
+"DropOffLocatiion: " + dropOffLocation + "<br>" +
+"AlternateSideParking: " + alternateSideParking + "<br>" +
+"Gas:" + gas + "<br>" +
+"Shrink Qty: " + shrinkQty + "<br>" +
+"Blanket Qty: " + blanketQty + "<br>" +
+"Tape Qty: "  + tapeQty + "<br>" +
+"Dolly Qty: " + dollyQty + "<br>" +
+"Right: " + right + "<br>" +
+"Left: " + left + "<br>" +
+"Right: " + right + "<br>" +
+"Left: " + left + "<br>" +
+"Cab Condition: " + cabCondition + "<br>" +
+"Back Condition: " + backCondition + "<br>" +
+"Exterior Truck Damage: " + exteriorTruckCondition + "<br>" +
+"Comments: " + comments + "<br>" 
 }
    	
 
